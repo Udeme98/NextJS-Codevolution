c286c186b3042c178f4c464cfa2e644bc7a414f3
Udeme98 <Darealudeme1@gmail.com>
2024-08-17 02:53:47 +0100

    Navigation - Active Links

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import "./style.css";
+
+type AuthLayoutProps = {
+  children: ReactNode;
+};
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+
+  return (
+    <div>
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+};
+export default AuthLayout;
