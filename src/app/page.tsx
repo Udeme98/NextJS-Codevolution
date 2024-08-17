4d98c4a24e0dfd626c16b92fbee5b66c29ed5a99
Udeme98 <Darealudeme1@gmail.com>
2024-08-17 08:37:13 +0100

    loading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h1>Home Page!</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Products</Link>
+      <Link href="/order-product">Order Products</Link>
     </>
   );
 };
