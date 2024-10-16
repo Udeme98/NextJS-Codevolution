c5891914e2f14347e9472101dad7c341c48cc37c
Udeme98 <Darealudeme1@gmail.com>
2024-10-16 07:06:16 +0100

    handling PATCH request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,11 +1,26 @@
+import { parse } from "path";
 import { comments } from "../data";
 
 export async function GET(
-  request: Request,
+  _request: Request,
   { params }: { params: { id: string } }
 ) {
   const commentFound = comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
   return Response.json(commentFound);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
 }
