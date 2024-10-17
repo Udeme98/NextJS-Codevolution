052f25ccad0ad71f80f009fe90598480aee28845
Udeme98 <Darealudeme1@gmail.com>
2024-10-17 03:12:09 +0100

    headers in route handlers

--- src/app/route-profile/api/route.ts
+++ src/app/route-profile/api/route.ts
@@ -1,3 +1,26 @@
-export async function GET() {
-  return new Response("Users Profile data!");
+//Method 1 - using request parameters
+
+// import { NextRequest } from "next/server";
+
+// export async function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization"));
+//   return new Response("Users Profile data!");
+// }
+
+//Method 2 - using headless function parameters
+
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(headerList.get("Authorization"));
+  return new Response("<h2>Users Profile data!</h2>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
