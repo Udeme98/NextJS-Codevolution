d46dc35f83ecab6dfb4715f2c16830e0ffb8e0d1
Udeme98 <Darealudeme1@gmail.com>
2024-10-17 09:40:12 +0100

    cookies in route handlers

--- src/app/route-profile/api/route.ts
+++ src/app/route-profile/api/route.ts
@@ -10,17 +10,25 @@
 
 //Method 2 - using headless function parameters
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
 
+  const theme = request.cookies.get("theme"); //first way of setting up a cookie
+  cookies().set("resultPage", "20"); //second way of setting up a cookie
+
   console.log(headerList.get("Authorization"));
+
+  console.log(theme); //reading the cookie in first method
+  console.log(cookies().get("resultPage")); //reading the cookie in second method
+
   return new Response("<h2>Users Profile data!</h2>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
