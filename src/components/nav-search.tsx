97cb05ac8ae8fdbb04f82145e26ba31234ffa379
Udeme98 <Darealudeme1@gmail.com>
2024-10-21 05:31:57 +0100

    client component placement

--- src/components/nav-search.tsx
+++ src/components/nav-search.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 const NavSearch = () => {
+  const [search, setSearch] = useState("");
   console.log("NavSearch rendered");
+
   return <div>Nav Search Input</div>;
 };
 export default NavSearch;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,9 @@
-"use client";
-
-import { useState } from "react";
 import NavLinks from "./nav-links";
 import NavSearch from "./nav-search";
 
 const Navbar = () => {
   console.log("Navbar Rendered");
-  const [search, setSearch] = useState("");
+
   return (
     <div>
       <NavLinks />
